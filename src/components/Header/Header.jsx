0a08138e640a5e7ceb92a4e0ee3a187d02175482
Filip <filip.knias@o2.pl>
2022-03-09 15:56:22 +0100

    add header-container class to center content in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import "./Header.css";
 import SearchBar from '../SearchBar/SearchBar';
+import FiltersPanel from '../FiltersPanel/FiltersPanel';
 
 const Header = () => {
   return (
     <div className="header">
-      <nav className="navbar">
-        <h1 className="navbar-header">News Tracker</h1>  
-        <ul className="navbar-nav">
-          <li className="nav-item active">News</li>
-          <li className="nav-item">Trending</li>
-          <li className="nav-item">Sources</li>
-        </ul>  
-      </nav>
-      <h1 className="header-text">Search for news</h1>
-      <p className="current-date-text">
-        <span className="date-divider">Monday</span>
-        07.03.2022  
-      </p>
-      <SearchBar /> 
+      <div className="header-container">
+        <nav className="navbar">
+          <h1 className="navbar-header">News Tracker</h1>  
+          <ul className="navbar-nav">
+            <li className="nav-item active">News</li>
+            <li className="nav-item">Trending</li>
+            <li className="nav-item">Sources</li>
+          </ul>  
+        </nav>
+        <h1 className="header-text">Search for news</h1>
+        <p className="current-date-text">
+          <span className="date-divider">Monday</span>
+          07.03.2022  
+        </p>
+        <SearchBar />
+        <FiltersPanel />
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./Home.css";
 import Article from '../../components/Article/Article';
-import FiltersButton from '../../components/FiltersButton/FiltersButton';
 
 const Home = () => {
   return (
-    < >
-      <FiltersButton />
+    <>
       <Article />
     </>
   )
