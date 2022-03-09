8e0663f0fa6ab74c111fe23fe30c09faf90e535c
Filip <filip.knias@o2.pl>
2022-03-09 13:33:21 +0100

    Add styles file to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import "./Home.css";
+import "./Home.css";
 import Article from '../../components/Article/Article';
 
 const Home = () => {
   return (
-    <div style={{ width: "60%", margin: "50px auto" }}>
+    <div className="home-container">
       <Article />
     </div>
   )
