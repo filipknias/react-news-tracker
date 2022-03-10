b34d6fd86102f0a62110f083ed30013211ab57e4
Filip <filip.knias@o2.pl>
2022-03-10 18:09:20 +0100

    Dropdown component created

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react';
+import "./Dropdown.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+const Dropdown = ({ children, currentValue }) => {
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef();
+
+  useEffect(() => {
+    window.onclick = (e) => {
+      const dropdownEl = dropdownRef.current;
+      if (!dropdownEl.contains(e.target)) {
+        setOpen(false);
+      };
+    };
+  }, []);
+
+  return (
+    <div className="dropdown-container" ref={dropdownRef}>
+      <div className="dropdown-trigger" onClick={() => setOpen(!open)}>
+        <p>{currentValue}</p>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </div>
+      <div className={`dropdown ${open ? 'dropdown-opened' : 'dropdown-closed'}`}>
+        {children}
+      </div>
+    </div>
+  )
+};
+
+const DropdownItem = ({ children, onClick }) => {
+  return (
+    <div className="dropdown-item" onClick={onClick ? onClick : undefined}>{children}</div>
+  )
+};
+Dropdown.Item = DropdownItem;
+
+export default Dropdown;
--- src/components/FiltersWrapper/FiltersWrapper.jsx
+++ src/components/FiltersWrapper/FiltersWrapper.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./FiltersWrapper.css";
+import Dropdown from '../Dropdown/Dropdown';
 
 const FiltersWrapper = () => {
   return (
     <div className="filters-wrapper">
-      
+      <Dropdown currentValue="Onet.pl">
+        <Dropdown.Item>Interia.pl</Dropdown.Item>
+        <Dropdown.Item>Interia.pl</Dropdown.Item>
+        <Dropdown.Item>Interia.pl</Dropdown.Item>
+      </Dropdown>
     </div>
   )
 }
