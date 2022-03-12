a9b94cab906fbc2521ba221fb1f6210701b8af1c
Filip <filip.knias@o2.pl>
2022-03-12 19:06:14 +0100

    Fetch articles in home page and add functions to useArticles hook

--- src/hooks/useArticles.jsx
+++ src/hooks/useArticles.jsx
@@ -1,20 +1,37 @@
 import React, { useContext } from 'react';
 import { ArticlesContext } from '../context/ArticlesProvider';
+import { fetchAllArticles } from '../api';
 
 function useArticles() {
   const [state, setState] = useContext(ArticlesContext);
 
+  // Getters
   const getArticles = () => state.articles;
   const getFilters = () => state.filters;
 
-  const updateFilters = (filters) => {
+  // Setters
+  const setFilters = (filters) => {
     setState((state) => { return { ...state, filters: { ...state.filters, ...filters } } });
+  };
+  const setArticles = (articles) => {
+    setState((state) => { return { ...state, articles } });
+  };
+
+  // Actions
+  const fetchArticles = async (filters) => {
+    if (filters) {
+      // TODO: Fetch articles with given filters
+    } else {
+      const { articles } = await fetchAllArticles();
+      setArticles(articles);
+    }
   };
 
   return {
     getArticles,
     getFilters,
-    updateFilters,
+    setFilters,
+    fetchArticles,
   };
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Home.css";
 import Article from '../../components/Article/Article';
+import useArticles from '../../hooks/useArticles';
 
 const Home = () => {
+  const { fetchArticles, getArticles } = useArticles();
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
   return (
     <>
-      <Article />
+      <div className="articles-container">
+        {getArticles().map((article) => (
+          <Article 
+            key={article.title}
+            title={article.title}
+            description={article.description}
+            photo={article.urlToImage}
+            author={article.author}
+            publishedAt={article.publishedAt}
+            source={article.source.name}
+            url={article.url}
+          />
+        ))}
+      </div>
     </>
   )
 }
