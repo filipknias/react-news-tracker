1e331d2f090739a3940a67f531a56afccba2d9be
Filip <filip.knias@o2.pl>
2022-03-12 19:02:17 +0100

    Add api file to seperate fetching api logic

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+const baseUrl = 'https://newsapi.org/v2';
+const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+const pageSize = 5;
+
+export const fetchAllArticles = async () => {
+  try {
+    // Get ip address
+    const ipApiUrl = 'http://api.ipapi.com';
+    const ipApiKey = process.env.REACT_APP_IP_API_KEY;
+    const ipResponse = await axios.get(`${ipApiUrl}/check?access_key=${ipApiKey}`);
+    const countryCode = ipResponse.data.country_code.toLowerCase();
+    // Fetch articles from country
+    const endpoint = `${baseUrl}/top-headlines?apiKey=${apiKey}&country=${countryCode}&pageSize=${pageSize}`;
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch (err) {
+    console.log(err.response);
+    return err;
+  }
+};
