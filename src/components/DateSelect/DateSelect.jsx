4e5cd24b51bfb7348e69c16897f692c3225bb4fe
Filip <filip.knias@o2.pl>
2022-03-17 19:56:16 +0100

    Add onChange prop to DateSelect component

--- src/components/DateSelect/DateSelect.jsx
+++ src/components/DateSelect/DateSelect.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import "./DateSelect.css";
 
-const DateSelect = ({ label }) => {
+const DateSelect = ({ label, onChange }) => {
   return (
     <div>
       <h2 className="filter-label">{label}</h2>
-      <input type="date" className="custom-date-input" />
+      <input type="date" className="custom-date-input" onChange={onChange ? onChange : undefined} />
     </div>
   )
 }
